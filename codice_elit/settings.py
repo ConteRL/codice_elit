6d81d560cbde4ebcc097be6eb656c8dfee0da367
ConteRL <rodrigo.oliveira2003@outlook.com>
2023-07-22 20:47:34 -0300

    First changes

--- codice_elit/settings.py
+++ codice_elit/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-#@209_ngus^^1cy)hu4gc=!aoyae)!&)@&^mr^cq2_-sj@$qe0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = "UTC"
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
